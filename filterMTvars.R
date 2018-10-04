823e7c5033add4c28b0e089569b5a254b715eded
ttriche <tim.triche@gmail.com>
2018-10-04 17:30:51 -0400

    add bulk ATAC BAMs and variant calls

---
+++ filterMTvars.R
@@ -0,0 +1,31 @@
+library(MTseeker)
+
+#' sanitize mitochondrial variant calls to a moderate degree
+#'
+#' @param vars 	an MVRanges or MVRangesList (will be unlisted and relisted)
+#' @param fp 	standard issue false positive filters? (TRUE: use Triska & RSRS)
+#' @param NuMT	variants with VAF < this number will be presumed NuMTs (0.03)
+#' @param covg	minimum median read coverage across chrM to be considered (20) 
+#'
+#' @return	a filtered set of variants 
+#' 
+#' @import 	GenomicRanges
+#' 
+#' @export 
+filterMTvars <- function(vars, fp=TRUE, NuMT=0.03, covg=20) {
+
+  if (fp) { 
+    data(fpFilter_RSRS, package="MTseeker")  
+    data(fpFilter_Triska, package="MTseeker")
+    fpRegions <- reduce(c(fpFilter_RSRS, fpFilter_Triska))
+    fpFilter <- subset(gaps(fpRegions), strand == "*")
+  } else { 
+    fpFilter <- GRanges("chrM", IRanges(start=1, end=16569), strand="*")
+  } 
+
+  stop("not finished yet") 
+
+}
+
+
+
